6760b59160cb4285a57e1e0134b84b158268cd1a
Helaly96 <zfaesu@gmail.com>
2020-05-31 06:34:05 +0200

    Fixed the touch code

--- Assets/BallLogic.cs
+++ Assets/BallLogic.cs
@@ -37,7 +37,7 @@ public class BallLogic : MonoBehaviour
         
         GameObject x= Instantiate(Ball, Spawnpoints[(int)spawn_point].transform.position, Spawnpoints[(int)spawn_point].transform.rotation);
         float rand_x = Random.Range(Corners[0].gameObject.transform.position.x, Corners[3].gameObject.transform.position.x);
-        float rand_y = Random.Range(Corners[1].gameObject.transform.position.y, Corners[2].gameObject.transform.position.x);
+        float rand_y = Random.Range(Corners[0].gameObject.transform.position.y, Corners[2].gameObject.transform.position.x);
         x.GetComponent<Check>().target = new Vector2(rand_x, rand_y);
         Vector2 dir = new Vector3(rand_x, rand_y) - Spawnpoints[(int)spawn_point].transform.position;
         x.GetComponent<Rigidbody2D>().AddForce(dir * 170);
--- Assets/Check.cs
+++ Assets/Check.cs
@@ -1,16 +1,41 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Check : MonoBehaviour
 {
     public Vector2 target;
+    bool Collided_with_player = false;
+    private Text Score_text;
+    public GameObject Particles;
+    private GameObject PS;
+
+    private void Start()
+    {
+        Score_text = GameObject.FindGameObjectWithTag("Score").GetComponent<Text>();
+        PS         = GameObject.FindGameObjectWithTag("SS");
+    }
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if ((collision.gameObject.CompareTag("Player")))
+        {
+            Collided_with_player = true;
+        }
+    }
 
     private void OnDestroy()
     {
-        if( Vector2.Distance(this.transform.position,target) > 0.4f)
+        if ( !(Collided_with_player))
         {
-            Debug.Log("GOAL!");
+            int score = int.Parse(Score_text.text);
+            score += 1;
+            Score_text.text = score.ToString();
+            GameObject x = Instantiate(Particles, PS.transform.position, PS.transform.rotation);
+            Destroy(x, 0.1f);
+
         }
+
+  
     }
 }
